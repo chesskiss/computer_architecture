17798afbdf44c52bcfb1c5988b75893b2365025f
akapelyan <alk246@cornell.edu>
2023-11-13 20:48:43 -0500

    Complete datapath with some minor errors

--- lab3/lab3_cache/CacheBaseDPath.v
+++ lab3/lab3_cache/CacheBaseDPath.v
@@ -27,12 +27,14 @@ module cacheBaseDpath (
     input logic data_array_w_en,
     input logic data_array_r_en,
 
-    input logic [4:0] line_counter,
-    input logic d_array_write_mux_sel,
+    input logic [4:0] byte_counter,
+    input logic data_array_write_mux_sel,
 
+
+    input logic tag_array_w_en,
 
         
-    output logic tarray_match,
+    output logic tag_array_match,
     output logic [31:0] dirty_bits,
     output logic [2:0] cache_req_type
 );
@@ -70,7 +72,7 @@ end
 
 
 
-logic [tag_bits-1:0] tag_addr;
+logic [tag_bits-1:0] tag_val;
 logic [index_bits-1:0] index; 
 logic [word_offset_bits:0] w_offset; 
 logic [byte_offset_bits-1:0] b_offset; 
@@ -110,10 +112,49 @@ assign index = memreq_msg_reg.addr[10:6];
 assign w_offset = memreq_msg_reg.addr[5:2];
 assign b_offset = memreq_msg_reg.addr[1:0];
 
-logic [63:0] one_hot_line_encodings; 
+logic [tag_bits-1:0] read_tag_data;
+
+vc_CombinationalBitSRAM_1rw #(21, 32) tag_array
+(
+    .clk           (clk),
+    .reset         (reset),
+
+    .read_en       (tag_array_en),
+    .read_addr     (index),
+    .read_data     (read_tag_data),
+
+    .write_en      (tag_array_w_en),
+    .write_addr    (index),
+    .write_data    (tag_val)
+);
+
+
+//Tag comparison
+assign tag_array_match = (read_tag_data == tag_val);
+
+
+
+logic [5:0] shift_amount = byte_counter*4;
+logic [63:0] one_hot_line_encodings = 64'd15 << shift_amount; 
+
 logic [511:0] read_data_value;
 logic [511:0] write_data_value;
 
+
+
+
+logic [31:0] data_array_val_32;
+logic [511:0] data_array_write_value;
+
+vc_Mux2 #(32) data_array_write_mux
+(
+.in0 (memreq_msg_reg.data),
+.in1 (cache_resp_msg_reg.data),
+.sel (data_array_write_mux_sel),
+.out (data_array_val_32)
+);
+
+assign data_array_write_value = {16{data_array_val_32}};
 
 
 
@@ -130,10 +171,44 @@ vc_CombinationalSRAM_1rw #(512, 32) data_array
     .write_en      (data_array_w_en),
     .write_byte_en (one_hot_line_encodings),
     .write_addr    (index),
-    .write_data    (darray_write)
+    .write_data    (data_array_write_value)
 );
 
 
+  vc_Mux16 #(32) read_word_mux
+  (
+    .in0  (read_data_data[31:0]),
+    .in1  (read_data_data[63:32]),
+    .in2  (read_data_data[95:64]),
+    .in3  (read_data_data[127:96]),
+    .in4  (read_data_data[159:128]),
+    .in5  (read_data_data[191:160]),
+    .in6  (read_data_data[223:192]),
+    .in7  (read_data_data[255:224]),
+    .in8  (read_data_data[287:256]),
+    .in9  (read_data_data[319:288]),
+    .in10 (read_data_data[351:320]),
+    .in11 (read_data_data[383:352]),
+    .in12 (read_data_data[415:384]),
+    .in13 (read_data_data[447:416]),
+    .in14 (read_data_data[479:448]),
+    .in15 (read_data_data[511:480]),
+    .sel  (w_offset),
+    .out  (read_word)
+  );
+
+
+
+
+// assign cache_req_msg.addr
+assign cache_req_msg.len = cache_resp_msg.len;
+assign cache_req_msg.data = read_word;
+
+assign memresp_msg.type_ = memreq_msg.type;
+assign memresp_msg.opaque = memreq_msg.opaque;
+
+assign memresp_msg.len = memreq_msg.len;
+assign memresp_msg.data = read_word;
 
 
 endmodule 
